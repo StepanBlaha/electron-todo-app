c63c6abb476a93dab9929361d16cb89359383ce6
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-03-21 21:36:45 +0100

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+    // Creates a new window object
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  //loads content into the window
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    // calls the window when the app is ready
+  createWindow()
+    // creates a new window when the app is activated
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+//quits the app when all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
